0c969eef880e2870a18a703568da29a73280abb9
bayupra7ama <pra7amabayu@gmail.com>
2024-03-07 22:12:26 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myrecycleview/ListHetoAdapter.kt
@@ -0,0 +1,59 @@
+package com.example.myrecycleview
+
+import android.view.LayoutInflater
+import android.view.ScrollCaptureCallback
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+
+class ListHeroAdapter(private var listHero:ArrayList<Hero>) : RecyclerView.Adapter<ListHeroAdapter.ListViewHolder>() {
+
+    private lateinit var onItemClickCallback: OnItemClickCallback
+
+
+//agar on click dapat ditriger dari mainActivity
+    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback
+    }
+
+
+    //listVIewHolder inheritech ke recycleView.Adapter
+    //harus pakai parameter constructor
+    class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imgPhoto : ImageView = itemView.findViewById(R.id.img_item_photo)
+        val tvName : TextView = itemView.findViewById(R.id.tv_item_name)
+        val tvDescription : TextView = itemView.findViewById(R.id.tv_item_description)
+
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_row_hero, parent, false)
+        return ListViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        val (name, description, photo) = listHero[position]
+        Glide.with(holder.itemView.context)
+            .load(photo)//img gambar
+            .into(holder.imgPhoto)
+        holder.tvName.text = name
+        holder.tvDescription.text = description
+
+        holder.itemView.setOnClickListener{onItemClickCallback.onItemCliced(listHero[holder.adapterPosition])}
+
+    }
+
+
+//agar click dapat ditriger di activity main
+    interface OnItemClickCallback {
+        fun onItemCliced(data : Hero)
+
+    }
+
+
+    override fun getItemCount(): Int = listHero.size
+}
